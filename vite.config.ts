a94d9a55fc3f4346d11715bb9504e61ee9f9096e
fordecember <1921567337@qq.com>
2025-03-10 16:19:35 +0800

    update: 添加组件依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
 
-// https://vite.dev/config/
+import path from 'path';
+
+const pathSrc = path.resolve(__dirname, "src");
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler" // or 'modern'
+      }
+    }
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [],
+      imports: [
+        'vue',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar'
+          ]
+        }
+      ]
+    }),
+    Components({
+      resolvers: [ NaiveUiResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": pathSrc,
+    },
+  }
 })
